ff17643ba544887fc94b2c68649dd61331573ff8
Matthew James Davis <davis.matthewjames@gmail.com>
2014-03-10 19:25:03 +0900

    amend configuration for static files add static files in separate directory

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 from flask import Flask, redirect, url_for, render_template
  
 application = Flask(__name__, 
-	static_url_path='',
-	static_folder='',
-	template_folder='html')
+	static_url_path='/static',
+	static_folder='static',
+	template_folder='static/html')
  
 @application.route('/')
 def hello_world():
     return render_template('index.html')
  
 if __name__ == '__main__':
-    application.run()
+    application.run(debug=True)
